cf6c95bf87d08e1920f53d4d807eea388e9885f4
milim <mylymkooleg42@gmail.com>
2025-05-08 19:54:00 +0200

    04.05.25

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from bot.bot_handler import telegram_webhook
 urlpatterns = [
     path('', views.bot_home, name='home'),
+    path('webhook/', telegram_webhook, name='telegram_webhook'),
+
 ]
